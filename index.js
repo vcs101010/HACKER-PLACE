4984eb369f99440aa83c7dc721a6802e6de02820
vcs101010 <victor.cardoso.silva10@escola.pr.gov.br>
2025-11-23 14:43:03 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function mostrarInstrucoes() {
+    document.getElementById("pagina-inicial").style.display = "none";
+    document.getElementById("pagina-instrucoes").style.display = "block";
+}
+
+function voltar() {
+    document.getElementById("pagina-inicial").style.display = "block";
+    document.getElementById("pagina-instrucoes").style.display = "none";
+}
